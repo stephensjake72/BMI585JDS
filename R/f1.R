df79d09389fecc65e2e6a4e720c9022296de6ccb
stephensjake72 <45493756+stephensjake72@users.noreply.github.com>
2021-12-09 21:06:13 -0500

    Update f1.R

--- R/f1.R
+++ R/f1.R
@@ -2,11 +2,11 @@ f1 = function(pred, truth){
   # load predictions and truth as logicals
   pred = as.logical(pred)
   truth = as.logical(truth)
-  
+
   # get positive predictive value and sensitivity
   precision = ppv(pred, truth)
   recall = sensitivity(pred, truth)
-  
+
   # return f1 score
   return(2*precision*recall/(precision + recall))
 }
